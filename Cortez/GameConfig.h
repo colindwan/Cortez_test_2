9f5d5ac91042305276c0df7b0e9a0ece80b5046c
COLIN DWAN <thedwans@colin-dwans-imac-2.local>
2011-04-12 20:47:01 -0400

    Initial Commit

---
+++ Cortez/GameConfig.h
@@ -0,0 +1,28 @@
+//
+//  GameConfig.h
+//  Cortez
+//
+//  Created by COLIN DWAN on 4/12/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#ifndef __GAME_CONFIG_H
+#define __GAME_CONFIG_H
+
+//
+// Supported Autorotations:
+//		None,
+//		UIViewController,
+//		CCDirector
+//
+#define kGameAutorotationNone 0
+#define kGameAutorotationCCDirector 1
+#define kGameAutorotationUIViewController 2
+
+//
+// Define here the type of autorotation that you want for your game
+//
+#define GAME_AUTOROTATION kGameAutorotationUIViewController
+
+
+#endif // __GAME_CONFIG_H
